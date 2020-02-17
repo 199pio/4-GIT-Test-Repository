0efa18645d8c946e8ed1d49fd571ba0514557e27
Mariusz Jarzynski <jarzynski31@gmail.com>
2020-02-17 23:28:42 +0000

    commit no 2
    this is second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        // TODO 1 to jest pierwszy commit
+        // TODO 1 to jest commit nr 1
+        // TODO 2 to jest commit nr 2
     }
 }
