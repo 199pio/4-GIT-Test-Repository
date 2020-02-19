91f45f198fff39b1f427d80235a6ccfd1b44cc62
Pio <199pio@gmail.com>
2020-02-19 23:11:14 +0000

    commit no 8
    this is commit from another account

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         // TODO 1 to jest commit nr 1
         // TODO 2 to jest commit nr 2
         // TODO 7 to jest commit no 7
+        // TODO 8 to jest commit no 8 z drugiego konta
 
     }
 }
