f8db2c7d62ca28a6407377be3f74ea7f47e0f48e
Mariusz Jarzynski <jarzynski31@gmail.com>
2020-02-19 22:02:34 +0000

    commit no 3
    this commit was made localy

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         // TODO 1 to jest commit nr 1
         // TODO 2 to jest commit nr 2
         // TODO 3 this file was changed in Github website
+        // TODO 4 this changes was made localy
     }
 }
